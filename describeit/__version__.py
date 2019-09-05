1b6e710b59e09abf862994c4b7b4cc1d89348bec
Michael Hucka <mhucka@caltech.edu>
2019-09-05 16:39:53 -0700

    Add initial version file

---
+++ describeit/__version__.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Describe It! version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/describeit
+# =============================================================================
+
+__version__          = '0.1'
+__title__            = 'describeit'
+__name__             = 'Describe It!'
+__description__      = 'A program to help Caltech Librarians do inventory.'
+__keywords__         = 'TIND MARC library-catalogues library'
+__url__              = 'https://github.com/caltechlibrary/describeit'
+__source_url__       = 'https://github.com/caltechlibrary/describeit'
+__download_url__     = 'https://github.com/caltechlibrary/describeit/releases'
+__author__           = 'Michael Hucka <mhucka@caltech.edu>'
+__email__            = 'mhucka@caltech.edu'
+__maintainer__       = 'Michael Hucka <mhucka@caltech.edu>'
+__maintainer_email__ = 'mhucka@caltech.edu'
+__license__          = 'BSD 3-clause license -- see LICENSE file'
+__copyright__        = 'Copyright (C) 2018-2019 by the California Institute of Technology'
