1583dfef7b46a180a912a0d56c978b044f7ad8c5
Michael Hucka <mhucka@caltech.edu>
2019-09-05 16:43:26 -0700

    Add initial __init__ file

---
+++ describeit/__init__.py
@@ -0,0 +1,21 @@
+'''
+Describe It! -- A program to help Caltech Librarians do inventory
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from .__version__ import __version__, __title__, __name__, __description__
+from .__version__ import __url__, __source_url__, __download_url__
+from .__version__ import __author__, __email__
+from .__version__ import __maintainer__, __maintainer_email__
+from .__version__ import __keywords__, __license__, __copyright__
