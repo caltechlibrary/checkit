d03327ec14062715b63f724d281c3a15168d6d2c
Michael Hucka <mhucka@caltech.edu>
2019-09-11 18:41:33 -0700

    Add initial versions of files taken from other programs

---
+++ checkit/files.py
@@ -0,0 +1,225 @@
+'''
+files.py: utilities for working with files.
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu> -- Caltech Library
+
+Copyright
+---------
+
+Copyright (c) 2019 by the California Institute of Technology.  This code is
+open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+import os
+from   os import path
+import shutil
+import subprocess
+import sys
+import tarfile
+import webbrowser
+import zipfile
+from   zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
+
+import checkit
+from checkit.debug import log
+
+
+# Constants.
+# .............................................................................
+
+_APP_NAME = 'Checkit'
+'''The human name of this application for human consumption.'''
+
+_APP_REG_PATH = r'Software\Caltech Library\Checkit\Settings'
+'''The Windows registry path for this application.'''
+
+
+# Main functions.
+# .............................................................................
+
+def readable(dest):
+    '''Returns True if the given 'dest' is accessible and readable.'''
+    return os.access(dest, os.F_OK | os.R_OK)
+
+
+def writable(dest):
+    '''Returns True if the destination is writable.'''
+    return os.access(dest, os.F_OK | os.W_OK)
+
+
+def module_path():
+    '''Returns the absolute path to our module installation directory.'''
+    # The path returned by module.__path__ is to the directory containing
+    # the __init__.py file.
+    return path.abspath(checkit.__path__[0])
+
+
+def installation_path():
+    '''Returns the path to where the application is installed.'''
+    # The path returned by module.__path__ is to the directory containing
+    # the __init__.py file.  What we want here is the path to the installation
+    # of the application binary.
+    if sys.platform.startswith('win'):
+        from winreg import OpenKey, CloseKey, QueryValueEx, HKEY_LOCAL_MACHINE, KEY_READ
+        try:
+            if __debug__: log('reading Windows registry entry')
+            key = OpenKey(HKEY_LOCAL_MACHINE, _APP_REG_PATH)
+            value, regtype = QueryValueEx(key, 'Path')
+            CloseKey(key)
+            if __debug__: log('path to windows installation: {}'.format(value))
+            return value
+        except WindowsError:
+            # Kind of a problem. Punt and return a default value.
+            return path.abspath('C:\Program Files\{}'.format(_APP_NAME))
+    else:
+        return path.abspath(path.join(module_path(), '..'))
+
+
+def desktop_path():
+    '''Returns the path to the user's desktop directory.'''
+    if sys.platform.startswith('win'):
+        return path.join(path.join(os.environ['USERPROFILE']), 'Desktop')
+    else:
+        return path.join(path.join(path.expanduser('~')), 'Desktop')
+
+
+def datadir_path():
+    '''Returns the path to Lost It's internal data directory.'''
+    return path.join(module_path(), 'data')
+
+
+def rename_existing(file):
+    '''Renames 'file' to 'file.bak'.'''
+
+    def rename(f):
+        backup = f + '.bak'
+        # If we fail, we just give up instead of throwing an exception.
+        try:
+            os.rename(f, backup)
+            if __debug__: log('renamed {} to {}', file, backup)
+        except:
+            try:
+                delete_existing(backup)
+                os.rename(f, backup)
+            except:
+                if __debug__: log('failed to delete {}', backup)
+                if __debug__: log('failed to rename {} to {}', file, backup)
+
+    if path.exists(file):
+        rename(file)
+        return
+    full_path = path.join(os.getcwd(), file)
+    if path.exists(full_path):
+        rename(full_path)
+        return
+
+
+def delete_existing(file):
+    '''Delete the given file.'''
+    # Check if it's actually a directory.
+    if path.isdir(file):
+        if __debug__: log('doing rmtree on directory {}', file)
+        try:
+            shutil.rmtree(file)
+        except:
+            if __debug__: log('unable to rmtree {}; will try renaming', file)
+            try:
+                rename_existing(file)
+            except:
+                if __debug__: log('unable to rmtree or rename {}', file)
+    else:
+        if __debug__: log('doing os.remove on file {}', file)
+        os.remove(file)
+
+
+def file_in_use(file):
+    if not path.exists(file):
+        return False
+    if sys.platform.startswith('win'):
+        # This is a hack, and it really only works for this purpose on Windows.
+        try:
+            os.rename(file, file)
+            return False
+        except:
+            return True
+    return False
+
+
+def open_file(file):
+    '''Open document with default application in Python.'''
+    # Code originally from https://stackoverflow.com/a/435669/743730
+    if __debug__: log('opening file {}', file)
+    if sys.platform.startswith('darwin'):
+        subprocess.call(('open', file))
+    elif os.name == 'nt':
+        os.startfile(file)
+    elif os.name == 'posix':
+        subprocess.call(('xdg-open', file))
+
+
+def open_url(url):
+    '''Open the given 'url' in a web browser using the current platform's
+    default approach.'''
+    if __debug__: log('opening url {}', url)
+    webbrowser.open(url)
+
+
+def make_dir(dir_path):
+    '''Creates directory 'dir_path' (including intermediate directories).'''
+    if path.isdir(dir_path):
+        if __debug__: log('Reusing existing directory {}', dir_path)
+        return
+    else:
+        if __debug__: log('Creating directory {}', dir_path)
+        # If this gets an exception, let it bubble up to caller.
+        os.makedirs(dir_path)
+
+
+def create_archive(archive_file, type, source_dir, comment = None):
+    root_dir = path.dirname(path.normpath(source_dir))
+    base_dir = path.basename(source_dir)
+    if type.endswith('zip'):
+        format = ZIP_STORED if type.startswith('uncompress') else ZIP_DEFLATED
+        current_dir = os.getcwd()
+        try:
+            os.chdir(root_dir)
+            with zipfile.ZipFile(archive_file, 'w', format) as zf:
+                for root, dirs, files in os.walk(base_dir):
+                    for file in files:
+                        zf.write(os.path.join(root, file))
+                if comment:
+                    zf.comment = comment.encode()
+        finally:
+            os.chdir(current_dir)
+    else:
+        if type.startswith('uncompress'):
+            shutil.make_archive(source_dir, 'tar', root_dir, base_dir)
+        else:
+            shutil.make_archive(source_dir, 'gztar', root_dir, base_dir)
+
+
+def verify_archive(archive_file, type):
+    '''Check the integrity of an archive and raise an exception if needed.'''
+    if type.endswith('zip'):
+        error = ZipFile(archive_file).testzip()
+        if error:
+            raise CorruptedContent('Failed to verify file "{}"'.format(archive_file))
+    else:
+        # Algorithm originally from https://stackoverflow.com/a/32312857/743730
+        tfile = None
+        try:
+            tfile = tarfile.open(archive_file)
+            for member in tfile.getmembers():
+                content = tfile.extractfile(member.name)
+                if content:
+                    for chunk in iter(lambda: content.read(1024), b''):
+                        pass
+        except Exception as ex:
+            raise CorruptedContent('Failed to verify file "{}"'.format(archive_file))
+        finally:
+            if tfile:
+                tfile.close()
---
+++ checkit/network.py
@@ -0,0 +1,280 @@
+'''
+network.py: miscellaneous network utilities for Handprint.
+'''
+
+import http.client
+from   http.client import responses as http_responses
+import requests
+from   requests.packages.urllib3.exceptions import InsecureRequestWarning
+from   time import sleep
+import ssl
+import urllib
+from   urllib import request
+import urllib3
+import warnings
+
+import checkit
+from   checkit.debug import log
+from   checkit.exceptions import *
+
+
+# Constants.
+# .............................................................................
+
+_MAX_RECURSIVE_CALLS = 10
+'''How many times can certain network functions call themselves upcon
+encountering a network error before they stop and give up.'''
+
+_MAX_FAILURES = 3
+'''Maximum number of network failures before we give up.'''
+
+_MAX_RETRIES = 5
+'''Maximum number of times we back off and try again.  This also affects the
+maximum wait time that will be reached after repeated retries.'''
+
+
+# Main functions.
+# .............................................................................
+
+def network_available():
+    '''Return True if it appears we have a network connection, False if not.'''
+    r = None
+    try:
+        r = urllib.request.urlopen("http://www.google.com")
+        return True
+    except Exception:
+        if __debug__: log('could not connect to https://www.google.com')
+        return False
+    if r:
+        r.close()
+
+
+def timed_request(get_or_post, url, session = None, timeout = 20, **kwargs):
+    '''Perform a network "get" or "post", handling timeouts and retries.
+    If "session" is not None, it is used as a requests.Session object.
+    "Timeout" is a timeout (in seconds) on the network requests get or post.
+    Other keyword arguments are passed to the network call.
+    '''
+    failures = 0
+    retries = 0
+    error = None
+    while failures < _MAX_FAILURES:
+        try:
+            with warnings.catch_warnings():
+                # The underlying urllib3 library used by the Python requests
+                # module will issue a warning about missing SSL certificates.
+                # We don't care here.  See also this for a discussion:
+                # https://github.com/kennethreitz/requests/issues/2214
+                warnings.simplefilter("ignore", InsecureRequestWarning)
+                if __debug__: log('doing http {} on {}', get_or_post, url)
+                if session:
+                    method = getattr(session, get_or_post)
+                else:
+                    method = requests.get if get_or_post == 'get' else requests.post
+                response = method(url, timeout = timeout, verify = False, **kwargs)
+                if __debug__: log('received {} bytes', len(response.content))
+                return response
+        except Exception as ex:
+            # Problem might be transient.  Don't quit right away.
+            if __debug__: log('exception: {}', str(ex))
+            failures += 1
+            # Record the first error we get, not the subsequent ones, because
+            # in the case of network outages, the subsequent ones will be
+            # about being unable to reconnect and not the original problem.
+            if not error:
+                error = ex
+        if failures >= _MAX_FAILURES:
+            # Try pause & continue, in case of transient network issues.
+            if retries < _MAX_RETRIES:
+                retries += 1
+                if __debug__: log('pausing because of consecutive failures')
+                sleep(60 * retries)
+                failures = 0
+            else:
+                # We've already paused & restarted once.
+                raise error
+
+
+def download_file(url, output_file, user = None, pswd = None, say = None):
+    if say:
+        say.info('Downloading {}'.format(url))
+    try:
+        download(url, user, pswd, output_file)
+        return True
+    except (NoContent, ServiceFailure, AuthFailure) as ex:
+        if say:
+            say.error('{}'.format(str(ex)))
+    return False
+
+
+def download(url, user, password, local_destination, recursing = 0):
+    '''Download the 'url' to the file 'local_destination'.'''
+    def addurl(text):
+        return (text + ' for {}').format(url)
+
+    try:
+        req = timed_request('get', url, stream = True, auth = (user, password))
+    except requests.exceptions.ConnectionError as ex:
+        if recursing >= _MAX_RECURSIVE_CALLS:
+            raise NetworkFailure(addurl('Too many connection errors'))
+        arg0 = ex.args[0]
+        if isinstance(arg0, urllib3.exceptions.MaxRetryError):
+            if __debug__: log(str(arg0))
+            original = unwrapped_urllib3_exception(arg0)
+            if isinstance(original, str) and 'unreacheable' in original:
+                return (req, NetworkFailure(addurl('Unable to connect to server')))
+            elif network_available():
+                raise NetworkFailure(addurl('Unable to resolve host'))
+            else:
+                raise NetworkFailure(addurl('Lost network connection with server'))
+        elif (isinstance(arg0, urllib3.exceptions.ProtocolError)
+              and arg0.args and isinstance(args0.args[1], ConnectionResetError)):
+            if __debug__: log('download() got ConnectionResetError; will recurse')
+            sleep(1)                    # Sleep a short time and try again.
+            recursing += 1
+            download(url, user, password, local_destination, recursing)
+        else:
+            raise NetworkFailure(str(ex))
+    except requests.exceptions.ReadTimeout as ex:
+        if network_available():
+            raise ServiceFailure(addurl('Timed out reading data from server'))
+        else:
+            raise NetworkFailure(addurl('Timed out reading data over network'))
+    except requests.exceptions.InvalidSchema as ex:
+        raise NetworkFailure(addurl('Unsupported network protocol'))
+    except Exception as ex:
+        raise
+
+    # Interpret the response.
+    code = req.status_code
+    if code == 202:
+        # Code 202 = Accepted, "received but not yet acted upon."
+        sleep(1)                        # Sleep a short time and try again.
+        recursing += 1
+        if __debug__: log('Calling download() recursively for http code 202')
+        download(url, user, password, local_destination, recursing)
+    elif 200 <= code < 400:
+        # This started as code in https://stackoverflow.com/a/13137873/743730
+        # Note: I couldn't get the shutil.copyfileobj approach to work; the
+        # file always ended up zero-length.  I couldn't figure out why.
+        with open(local_destination, 'wb') as f:
+            for chunk in req.iter_content(chunk_size = 1024):
+                if chunk:
+                    f.write(chunk)
+        req.close()
+    elif code in [401, 402, 403, 407, 451, 511]:
+        raise AuthFailure(addurl('Access is forbidden'))
+    elif code in [404, 410]:
+        raise NoContent(addurl('No content found'))
+    elif code in [405, 406, 409, 411, 412, 414, 417, 428, 431, 505, 510]:
+        raise InternalError(addurl('Server returned code {}'.format(code)))
+    elif code in [415, 416]:
+        raise ServiceFailure(addurl('Server rejected the request'))
+    elif code == 429:
+        raise RateLimitExceeded('Server blocking further requests due to rate limits')
+    elif code == 503:
+        raise ServiceFailure('Server is unavailable -- try again later')
+    elif code in [500, 501, 502, 506, 507, 508]:
+        raise ServiceFailure(addurl('Internal server error (HTTP code {})'.format(code)))
+    else:
+        raise NetworkFailure('Unable to resolve {}'.format(url))
+
+
+def net(get_or_post, url, session = None, polling = False, recursing = 0, **kwargs):
+    '''Gets or posts the 'url' with optional keyword arguments provided.
+    Returns a tuple of (response, exception), where the first element is
+    the response from the get or post http call, and the second element is
+    an exception object if an exception occurred.  If no exception occurred,
+    the second element will be None.  This allows the caller to inspect the
+    response even in cases where exceptions are raised.
+
+    If keyword 'session' is not None, it's assumed to be a requests session
+    object to use for the network call.
+
+    If keyword 'polling' is True, certain statuses like 404 are ignored and
+    the response is returned; otherwise, they are considered errors.
+    '''
+    def addurl(text):
+        return (text + ' for {}').format(url)
+
+    req = None
+    try:
+        req = timed_request(get_or_post, url, session, **kwargs)
+    except requests.exceptions.ConnectionError as ex:
+        if recursing >= _MAX_RECURSIVE_CALLS:
+            return (req, NetworkFailure(addurl('Too many connection errors')))
+        arg0 = ex.args[0]
+        if isinstance(arg0, urllib3.exceptions.MaxRetryError):
+            if __debug__: log(str(arg0))
+            original = unwrapped_urllib3_exception(arg0)
+            if isinstance(original, str) and 'unreacheable' in original:
+                return (req, NetworkFailure(addurl('Unable to connect to server')))
+            elif network_available():
+                raise NetworkFailure(addurl('Unable to resolve host'))
+            else:
+                raise NetworkFailure(addurl('Lost network connection with server'))
+        elif (isinstance(arg0, urllib3.exceptions.ProtocolError)
+              and arg0.args and isinstance(args0.args[1], ConnectionResetError)):
+            if __debug__: log('net() got ConnectionResetError; will recurse')
+            sleep(1)                    # Sleep a short time and try again.
+            return net(get_or_post, url, session, polling, recursing + 1, **kwargs)
+        else:
+            return (req, NetworkFailure(str(ex)))
+    except requests.exceptions.ReadTimeout as ex:
+        if network_available():
+            return (req, ServiceFailure(addurl('Timed out reading data from server')))
+        else:
+            return (req, NetworkFailure(addurl('Timed out reading data over network')))
+    except requests.exceptions.InvalidSchema as ex:
+        return (req, NetworkFailure(addurl('Unsupported network protocol')))
+    except Exception as ex:
+        return (req, ex)
+
+    # Interpret the response.  Note that the requests library handles code 301
+    # and 302 redirects automatically, so we don't need to do it here.
+    code = req.status_code
+    error = None
+    if code == 400:
+        error = RequestError('Server rejected the request')
+    elif code in [401, 402, 403, 407, 451, 511]:
+        error = AuthFailure(addurl('Access is forbidden'))
+    elif code in [404, 410] and not polling:
+        error = NoContent(addurl("No content found"))
+    elif code in [405, 406, 409, 411, 412, 414, 417, 428, 431, 505, 510]:
+        error = InternalError(addurl('Server returned code {}'.format(code)))
+    elif code in [415, 416]:
+        error = ServiceFailure(addurl('Server rejected the request'))
+    elif code == 429:
+        if recursing < _MAX_RECURSIVE_CALLS:
+            pause = 5 * (recursing + 1)   # +1 b/c we start with recursing = 0.
+            if __debug__: log('rate limit hit -- sleeping {}', pause)
+            sleep(pause)                  # 5 s, then 10 s, then 15 s, etc.
+            return net(get_or_post, url, session, polling, recursing + 1, **kwargs)
+        error = RateLimitExceeded('Server blocking further requests due to rate limits')
+    elif code == 503:
+        error = ServiceFailure('Server is unavailable -- try again later')
+    elif code in [500, 501, 502, 506, 507, 508]:
+        error = ServiceFailure('Dimensions server error (HTTP code {})'.format(code))
+    elif not (200 <= code < 400):
+        error = NetworkFailure("Unable to resolve {}".format(url))
+    return (req, error)
+
+
+# Next code originally from https://stackoverflow.com/a/39779918/743730
+def disable_ssl_cert_check():
+    requests.packages.urllib3.disable_warnings()
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+        # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+
+
+def unwrapped_urllib3_exception(ex):
+    if hasattr(ex, 'args') and isinstance(ex.args, tuple):
+        return unwrapped_urllib3_exception(ex.args[0])
+    else:
+        return ex
