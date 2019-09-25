1eb207d7743953a7e009106a7a468e7cbcc46bf3
Michael Hucka <mhucka@caltech.edu>
2019-09-24 20:23:36 -0700

    Refactor and move body to main_body.py

--- checkit/__main__.py
+++ checkit/__main__.py
@@ -14,27 +14,18 @@ is open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-import os
-import os.path as path
 import plac
 import sys
-import time
-from   threading import Thread
 
 import checkit
 from checkit.access import AccessHandlerGUI, AccessHandlerCLI
-from checkit.config import Config
-from checkit.control import CheckitControlGUI, CheckitControlCLI
-from checkit.debug import set_debug, log
-from checkit.email import Mailer
+from checkit.control import ControlGUI, ControlCLI
 from checkit.exceptions import *
-from checkit.files import module_path
-from checkit.google_sheet import Google
+from checkit.debug import set_debug, log
+from checkit.main_body import MainBody
 from checkit.messages import MessageHandlerGUI, MessageHandlerCLI
-from checkit.network import network_available, net
+from checkit.network import network_available
 from checkit.progress import ProgressIndicatorGUI, ProgressIndicatorCLI
-from checkit.records import records_diff, records_filter
-from checkit.tind import Tind
 
 
 # Main program.
@@ -46,16 +37,16 @@ from checkit.tind import Tind
     input_csv  = ('input file containing list of barcodes',             'option', 'i'),
     no_keyring = ('do not store credentials in a keyring service',      'flag',   'K'),
     output_csv = ('output file where results should be written as CSV', 'option', 'o'),
-    pswd       = ('Caltech access user password',                       'option', 'p'),
+    password   = ('Caltech access user password',                       'option', 'p'),
     reset_keys = ('reset user and password used',                       'flag',   'R'),
     user       = ('Caltech access user name',                           'option', 'u'),
     version    = ('print version info and exit',                        'flag',   'V'),
-    debug      = ('turn on debugging',                                  'flag',   '@'),
+    debug      = ('write detailed trace to "OUT" ("-" means console)',  'option', '@'),
 )
 
 def main(no_color = False, no_gui = False, input_csv = 'I', no_keyring = False,
-         output_csv = 'O', pswd = 'P', reset_keys = False, user = 'U',
-         version = False, debug = False):
+         output_csv = 'O', password = 'P', reset_keys = False, user = 'U',
+         version = False, debug = 'OUT'):
     '''Check It!'''
 
     # Initial setup -----------------------------------------------------------
@@ -69,182 +60,66 @@ def main(no_color = False, no_gui = False, input_csv = 'I', no_keyring = False,
 
     # Preprocess arguments and handle early exits -----------------------------
 
-    if debug:
-        set_debug(True)
+    if debug != 'OUT':
+        set_debug(True, debug)
     if version:
         print_version()
         sys.exit()
 
-    if input_csv == 'I' and use_gui:
-        input_csv = file_to_open(splitit.__title__ + ': open input CSV file',
-                                 wildcard = 'CSV file (*.csv)|*.csv|Any file (*.*)|*.*')
-        if input_csv is None:
-            exit('Quitting.')
-    else:
-        exit(say.error_text('Must supply input file using -i. {}'.format(hint)))
-    if not readable(input_csv):
-        exit(say.error_text('Cannot read file: {}'.format(input_csv)))
-    elif not is_csv(input_csv):
-        exit(say.error_text('File does not appear to contain CSV: {}'.format(input_csv)))
-
-    if output_csv == 'O' and use_gui:
-        output_csv = file_to_save(splitit.__title__ + ': save output file')
-        if output_csv is None:
-            exit('Quitting.')
-    else:
-        exit(say.error_text('Must supply output file using -o. {}'.format(hint)))
-    if path.exists(output_csv):
-        if file_in_use(output_csv):
-            exit(say.error_text('File is open by another application: {}'.format(output_csv)))
-        elif not writable(output_csv):
-            exit(say.error_text('Unable to write to file: {}'.format(output_csv)))
-    else:
-        dest_dir = path.dirname(output_csv) or os.getcwd()
-        if not writable(dest_dir):
-            exit(say.error_text('Cannot write to folder: {}'.format(dest_dir)))
-
-    if user == 'U':
-        user = None
-    if pswd == 'P':
-        pswd = None
+    user     = None if user == 'U' else user
+    password = None if password == 'P' else password
+    infile   = None if input_csv == 'I' else input_csv
+    outfile  = None if output_csv == 'O' else output_csv
 
     # Do the real work --------------------------------------------------------
 
-    if use_gui:
-        controller = CheckitControlGUI()
-        accesser   = AccessHandlerGUI(user, pswd)
-        notifier   = MessageHandlerGUI()
-        tracer     = ProgressIndicatorGUI()
-    else:
-        controller = CheckitControlCLI()
-        accesser   = AccessHandlerCLI(user, pswd, use_keyring, reset_keys)
-        notifier   = MessageHandlerCLI(use_color)
-        tracer     = ProgressIndicatorCLI(use_color)
-
-    # Start the worker thread.
-    if __debug__: log('starting main body thread')
-    controller.start(MainBody(input_csv, output_csv,
-                              controller, accesser, notifier, tracer, debug))
-
-
-class MainBody(Thread):
-    '''Main body of Check It! implemented as a Python thread.'''
-
-    def __init__(self, view_sheet, send_mail, debug,
-                 controller, accesser, notifier, tracer):
-        '''Initializes main thread object but does not start the thread.'''
-        Thread.__init__(self, name = "MainBody")
-        self._view_sheet = view_sheet
-        self._send_mail  = send_mail
-        self._debug      = debug
-        self._controller = controller
-        self._tracer     = tracer
-        self._accesser   = accesser
-        self._notifier   = notifier
-        if controller.is_gui:
-            # Only make this a daemon thread when using the GUI; for CLI, it
-            # must not be a daemon thread or else Check It! exits immediately.
-            self.daemon = True
-
-
-    def run(self):
-        # Set shortcut variables for better code readability below.
-        view_sheet = self._view_sheet
-        send_mail  = self._send_mail
-        debug      = self._debug
-        controller = self._controller
-        accesser   = self._accesser
-        notifier   = self._notifier
-        tracer     = self._tracer
-
-        # Preliminary sanity checks.  Do this here because we need the notifier
-        # object to be initialized based on whether we're using GUI or CLI.
-        tracer.start('Performing initial checks')
-        if not network_available():
-            notifier.fatal('No network connection.')
-
-        # Let's do this thing.
-        try:
-            config      = Config(path.join(module_path(), "checkit.ini"))
-            tind        = Tind(accesser, notifier, tracer)
-            google      = Google(accesser, notifier, tracer)
-
-            sid         = config.get('checkit', 'spreadsheet_id')
-            mail_server = config.get('checkit', 'mail_server')
-            mail_port   = config.get('checkit', 'mail_port')
-            recipients  = config.get('checkit', 'mail_recipients')
-
-            # Get the data from TIND and the Google spreadsheet of lost
-            # items, we look at the two first tabs.  The tab lookup is done
-            # by position, NOT by name; the names of the tabs make no
-            # difference.  Check It! assumes that the first tab is the current
-            # NOS list and the second tab is a list of historical records,
-            # but it doesn't care what the cut-off is between the tabs.  It
-            # merely gathers the records from both tabs.
-            tind_records = tind.records()
-            google_records = google.records(sid, tab = 0) + google.records(sid, tab = 1)
-
-            # Figure out what's new.
-            tracer.update('Comparing TIND records to our spreadsheet')
-            new_records = records_diff(google_records, tind_records)
-            num_new = len(new_records)
-            tracer.update('Found {} new records'.format(num_new))
-
-            # Update the Google spreadsheet with new records.
-            if num_new > 0:
-                new_records = sorted(new_records, key = lambda r: r.date_requested)
-                tracer.update('Updating Google spreadsheet')
-                google.update(sid, new_records)
-
-            # Open the spreadsheet, if requested.
-            if isinstance(notifier, MessageHandlerGUI):
-                if notifier.yes_no('Open the tracking spreadsheet?'):
-                    tracer.update('Opening the Google spreadsheet')
-                    google.open(sid)
-            elif view_sheet:
-                tracer.update('Opening the Google spreadsheet')
-                google.open(sid)
-
-            # Send mail, if requested.
-            if send_mail and num_new > 0:
-                tracer.update('Sending mail')
-                subject  = 'Check It! reports {} lost item{}'.format(
-                    num_new, 's' if num_new > 1 else '')
-                body     = email_body(new_records, google.spreadsheet_url(sid))
-                sender   = accesser.user + '@caltech.edu'
-                password = accesser.password
-                mailer   = Mailer(mail_server, mail_port)
-                mailer.send(sender, password, recipients, subject, body)
-
-        except (KeyboardInterrupt, UserCancelled) as err:
-            tracer.stop('Quitting.')
-            controller.stop()
-        except ServiceFailure:
-            tracer.stop('Stopping due to a problem connecting to services')
-            controller.stop()
-        except Exception as err:
-            import traceback
-            if debug:
-                tracer.stop('{}\n{}'.format(str(ex), traceback.format_exc()))
-                import pdb; pdb.set_trace()
-            else:
-                notifier.fatal(checkit.__title__ + ' encountered an error',
-                               str(err) + '\n' + traceback.format_exc())
-                tracer.stop('Stopping due to error')
-                controller.stop()
+    try:
+        if __debug__: log('initializing handlers')
+        if use_gui:
+            controller = ControlGUI('Check It!')
+            accessor   = AccessHandlerGUI(user, password)
+            notifier   = MessageHandlerGUI()
+            tracer     = ProgressIndicatorGUI()
         else:
-            tracer.stop('Done')
+            controller = ControlCLI('Check It!')
+            accessor   = AccessHandlerCLI(user, password, use_keyring, reset_keys)
+            notifier   = MessageHandlerCLI(use_color)
+            tracer     = ProgressIndicatorCLI(use_color)
+
+        if __debug__: log('starting main body thread')
+        body = MainBody(infile, outfile, controller, accessor, notifier, tracer, debug)
+        #controller.start(body)
+    except (KeyboardInterrupt, UserCancelled) as err:
+        tracer.stop('Quitting.')
+        controller.stop()
+    except ServiceFailure:
+        tracer.stop('Stopping due to a problem connecting to services')
+        controller.stop()
+    except Exception as err:
+        import traceback
+        if debug:
+            tracer.stop('{}\n{}'.format(str(ex), traceback.format_exc()))
+            import pdb; pdb.set_trace()
+        else:
+            notifier.fatal(__package__ + ' encountered an error',
+                           str(err) + '\n' + traceback.format_exc())
+            tracer.stop('Stopping due to error')
             controller.stop()
+    else:
+        tracer.stop('Done')
+        controller.stop()
+
 
 
 # Miscellaneous utilities.
 # .............................................................................
 
 def print_version():
-    print('{} version {}'.format(checkit.__title__, checkit.__version__))
-    print('Author: {}'.format(checkit.__author__))
-    print('URL: {}'.format(checkit.__url__))
-    print('License: {}'.format(checkit.__license__))
+    this_module = sys.modules[__package__]
+    print('{} version {}'.format(this_module.__name__, this_module.__version__))
+    print('Authors: {}'.format(this_module.__author__))
+    print('URL: {}'.format(this_module.__url__))
+    print('License: {}'.format(this_module.__license__))
 
 
 # Main entry point.
@@ -254,7 +129,7 @@ def print_version():
 if sys.platform.startswith('win'):
     main.prefix_chars = '/'
 
-# The following allows users to invoke this using "python3 -m handprint".
+# The following allows users to invoke this using "python3 -m checkit".
 if __name__ == '__main__':
     plac.call(main)
 
