471d244efd35be6df510ee4b5b389a5b47e641b4
Michael Hucka <mhucka@caltech.edu>
2019-09-26 14:50:53 -0700

    Use new combined tracer/notifier

--- checkit/main_body.py
+++ checkit/main_body.py
@@ -1,11 +1,13 @@
 import os
 import os.path as path
+import sys
 from   threading import Thread
 
 from   pubsub import pub
 import wx
 
 
+from .debug import log
 from .exceptions import *
 from .files import readable, writable, file_in_use
 from .network import network_available
@@ -14,8 +16,8 @@ from .network import network_available
 class MainBody(Thread):
     '''Main body of Check It! implemented as a Python thread.'''
 
-    def __init__(self, infile, outfile, controller, accessor, notifier, tracer, debug):
-        '''Initializes main thread object but does not start the thread.'''
+    def __init__(self, infile, outfile, controller, accessor, notifier):
+        '''Initializes main body thread object but does not start the thread.'''
         Thread.__init__(self, name = "MainBody")
 
         # Make this a daemon thread, but only when using the GUI; for CLI, it
@@ -27,66 +29,86 @@ class MainBody(Thread):
         # out if the thread finished normally or with an exception.
         self.exception = None
 
-        # The rest of this sets internal variables.
-        self._infile     = infile
-        self._outfile    = outfile
-        self._debug      = debug
-        self._controller = controller
-        self._tracer     = tracer
-        self._accessor   = accessor
-        self._notifier   = notifier
-        self._interrupted = False
+        # The rest of this sets internal variables used by other methods.
+        self._infile      = infile
+        self._outfile     = outfile
+        self._controller  = controller
+        self._accessor    = accessor
+        self._notifier    = notifier
 
 
     def run(self):
+        '''Run the main body.'''
+        # Set shortcut variables for better code readability below.
+        controller = self._controller
+        notifier   = self._notifier
+
+        # In normal operation, this method returns after things are done and
+        # leaves it to the user to exit the application via the control GUI.
+        # If exceptions occur, we capture the stack context for the caller
+        # to interpret, and force the controller to quit.
+        try:
+            notifier.info('Welcome to ' + controller.app_name)
+            self.main_body()
+            notifier.info('Done.')
+        except Exception as ex:
+            if __debug__: log('exception in main body')
+            self.exception = sys.exc_info()
+            controller.quit()
+
+
+    def stop(self):
+        '''Stop the main body thread.'''
+        if __debug__: log('stopping main body thread')
+        # Nothing to do for the current application.
+        pass
+
+
+    def main_body(self):
+        '''The main body.'''
+
         # Set shortcut variables for better code readability below.
         infile     = self._infile
         outfile    = self._outfile
-        debug      = self._debug
         controller = self._controller
         accessor   = self._accessor
         notifier   = self._notifier
-        tracer     = self._tracer
 
         # Preliminary sanity checks -------------------------------------------
 
-        tracer.start('Performing initial checks')
+        notifier.info('Performing initial checks')
+
         if not network_available():
             notifier.fatal('No network connection.')
             return
 
         if not infile and controller.is_gui:
-            tracer.update('Asking user for input file')
+            notifier.info('Asking user for input file')
             infile = controller.open_file('Open barcode file', 'CSV file|*.csv|Any file|*.*')
         if not infile:
-            tracer.update('No input file -- nothing to do')
+            notifier.warn('No input file -- nothing to do')
             return
         if not readable(infile):
-            tracer.update('Cannot read file: {}'.format(infile))
+            notifier.warn('Cannot read file: {}'.format(infile))
             return
 
         if not outfile and controller.is_gui:
-            tracer.update('Asking user for output file')
+            notifier.info('Asking user for output file')
             outfile = controller.save_file('Output destination file')
         if not outfile:
-            tracer.update('No output file specified -- cannot continue')
+            notifier.info('No output file specified -- cannot continue')
             return
         if path.exists(outfile):
             if file_in_use(outfile):
-                tracer.update('File is open by another application: {}'.format(outfile))
+                notifier.info('File is open by another application: {}'.format(outfile))
                 return
             elif not writable(outfile):
-                tracer.update('Unable to write to file: {}'.format(outfile))
+                notifier.info('Unable to write to file: {}'.format(outfile))
                 return
         else:
             dest_dir = path.dirname(outfile) or os.getcwd()
             if not writable(dest_dir):
-                tracer.update('Cannot write to folder: {}'.format(dest_dir))
+                notifier.info('Cannot write to folder: {}'.format(dest_dir))
                 return
 
         # Main work -----------------------------------------------------------
-
-
-
-    def stop(self):
-        pass
