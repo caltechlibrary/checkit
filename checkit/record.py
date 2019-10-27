0ab47b00551edbad7d3aa75535575c9080b37516
Michael Hucka <mhucka@caltech.edu>
2019-10-26 18:52:24 -0700

    Rename to BaseRecord + some cleanup

--- checkit/record.py
+++ checkit/record.py
@@ -1,5 +1,12 @@
 '''
-records.py: base record class for holding data from TIND
+record.py: base record class for items in TIND
+
+The BaseRecord object class defined in this file represents a subset of the
+data available directly in a record returned by our AJAX calls to
+Caltech.tind.io.  In other words, it's data that does not require additional
+computing or scraping of other pages to obtain.  The data pertains to a
+specific item (identified by a barcode) but it also has info about the TIND
+record that references this item.
 
 Authors
 -------
@@ -14,22 +21,18 @@ is open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
 
-from datetime import datetime
-
-from .debug import log
-
 
 # Global constants.
 # .............................................................................
 
 _ATTRIBUTE_TITLES = {
+    'item_barcode'       : 'Barcode',
     'item_title'         : 'Title',
     'item_author'        : 'Author',
     'item_type'          : 'Item type',
     'item_call_number'   : 'Call number',
     'item_copy_number'   : 'Copy number',
     'item_tind_id'       : 'TIND id',
-    'item_barcode'       : 'Barcode',
     'item_details_url'   : 'Details page',
     'item_record_url'    : 'Item record page',
     'item_location_name' : 'Location name',
@@ -39,40 +42,32 @@ _ATTRIBUTE_TITLES = {
     'item_loan_period'   : 'Loan period',
     'date_created'       : 'Date created',
     'date_modified'      : 'Date modified',
-    'requester_name'     : 'Requester name',
-    'requester_email'    : 'Requester email',
-    'requester_type'     : 'Patron type',
-    'requester_url'      : 'Requester details page',
-    'date_requested'     : 'Date requested',
-    'holdings_total'     : 'Total holdings'
 }
 '''Mapping of Python record object attributes to human-readable short
 descriptive titles for the attributes.'''
-
 
 
 # Class definitions.
 # .............................................................................
 
-class BaseRecord(object):
+class ItemRecord(object):
     '''Base class for records describing an item in TIND.io.  Note that the
     field names in this record object do not match exactly the field names in
     TIND; the reasons include (1) we add some additional non-item info to these
     records in subclasses, so naming the fields more clearly here makes coding
     easier later, and (2) some of the TIND field names are IMHO ambiguous, so
     I tried to name things in a way that made meaning more clear.  Also, not
-    all the fields available in TIND are represented here because Caltech
-    Library either doesn't use them or they always seem to be blank.'''
+    every field available in TIND is represented here because Caltech Library
+    either doesn't use them or they always seem to be blank.'''
 
     def __init__(self):
-        # The following map to the json objects returned by TIND ajax calls.
+        self.item_barcode = ''                 # barcode
         self.item_title = ''                   # title
         self.item_author = ''                  # extracted from title field
         self.item_type = ''                    # item_type
         self.item_call_number = ''             # call_no
         self.item_copy_number = ''             # description
         self.item_tind_id = ''                 # id_bibrec
-        self.item_barcode = ''                 # barcode
         self.item_details_url = ''             # links.barcode
         self.item_record_url = ''              # links.title
         self.item_location_name = ''           # location_name
@@ -80,24 +75,22 @@ class BaseRecord(object):
         self.item_holds_count = ''             # number_of_requests
         self.item_loan_status = ''             # status
         self.item_loan_period = ''             # loan_period
-
         self.date_created = ''                 # creation_date
         self.date_modified = ''                # modification_date
 
 
     def __repr__(self):
-        return '<{} {}>'.format(self.__class__.__name__, self.item_tind_id)
+        return '<{} {}>'.format(self.__class__.__name__, self.item_barcode)
 
 
     def __hash__(self):
-        return hash(self.item_barcode + self.requester_name + self.date_requested)
+        return hash(self.item_barcode)
 
 
     def __eq__(self, other):
-        return (self.item_barcode == other.item_barcode
-                and self.requester_name == other.requester_name
-                and self.date_requested == other.date_requested)
-
+        return (self.__class__ == other.__class__
+                and self.item_barcode == other.item_barcode)
+ 
 
     def __ne__(self, other):
         return not self.__eq__(other)
@@ -125,3 +118,16 @@ class BaseRecord(object):
         describes its meaning.'''
         if name in _ATTRIBUTE_TITLES:
             return _ATTRIBUTE_TITLES[name]
+
+
+    def as_string(self):
+        '''Return a string representing this record and all its attribute
+        values.  (Useful when debugging.)
+        '''
+        attr_value_pairs = []
+        for attr in dir(self):
+            if attr.startswith('item_') or attr.startswith('date_'):
+                attr_value_pairs.append(attr + '="' + str(getattr(self, attr)) + '"')
+        c_name = self.__class__.__name__
+        barcode = self.item_barcode
+        return '<{} {} {}>'.format(c_name, barcode, ' '.join(attr_value_pairs))
